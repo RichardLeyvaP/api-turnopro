45111ad9a081f50ff0c38986b7d8f46c7eb34c25
jimmbo89 <yasmany891230@gmail.com>
2023-12-06 09:24:38 -0500

    Update OrderService.php

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -10,31 +10,11 @@ use Carbon\Carbon;
 
 class OrderService {
     public function product_order_store($data){
-        /*$clientprofessional = ClientProfessional::where('client_id',$data['client_id'])->where('professional_id',$data['professional_id'])->first();
-            if (!$clientprofessional) {
-                $clientprofessional = new ClientProfessional();
-                $clientprofessional->client_id = $data['client_id'];
-                $clientprofessional->professional_id = $data['professional_id'];
-                $clientprofessional->save();
-            }
-            $client_professional_id = $clientprofessional->id;
-            $productcar = Car::where('client_professional_id', $client_professional_id)->whereDate('updated_at', Carbon::today())->first();*/
-            //$car = Car::find($data['car_id'])->firts();
             $car = Car::findOrFail($data['car_id']);
             $productStore = ProductStore::with('product')->where('id', $data['product_id'])->first();
                 $sale_price = $productStore->product()->first()->sale_price;
-                //if ($productcar) {
-                    //$car = Car::find($productcar->id);
                     $car->amount = $car->amount + $sale_price;
-                //}
-                /*else {
-                    $car = new Car();
-                    $car->client_professional_id = $client_professional_id;
-                    $car->amount = $sale_price;
-                    $car->pay = false;
-                    $car->active = false;
-                }*/
-                $car->save();
+                    $car->save();
                 $car_id = $car->id;
                     //rebajar la existencia
                 $productstore = ProductStore::find($data['product_id']);
@@ -54,30 +34,10 @@ class OrderService {
         return $order;
     }
     public function service_order_store($data){
-        /*$clientprofessional = ClientProfessional::where('client_id',$data['client_id'])->where('professional_id',$data['professional_id'])->first();
-            if (!$clientprofessional) {
-                $clientprofessional = new ClientProfessional();
-                $clientprofessional->client_id = $data['client_id'];
-                $clientprofessional->professional_id = $data['professional_id'];
-                $clientprofessional->save();
-            }
-            $client_professional_id = $clientprofessional->id;*/
-            //$productcar = Car::where('client_professional_id', $client_professional_id)->whereDate('updated_at', Carbon::today())->first();
-            //$car = Car::find($data['car_id'])->firts();
             $car = Car::findOrFail($data['car_id']);
             $branchServiceprofessional = BranchServiceProfessional::with('branchService.service')->where('id', $data['service_id'])->first();
                 $service = $branchServiceprofessional->branchService->service;
-                //if ($productcar) {
-                    //$car = Car::find($productcar->id);
                     $car->amount = $car->amount + $service->price_service+$service->profit_percentaje/100;
-                //}
-                /*else {
-                    $car = new Car();
-                    $car->client_professional_id = $client_professional_id;
-                    $car->amount = $service->price_service+$service->profit_percentaje/100;
-                    $car->pay = false;
-                    $car->active = false;
-                }*/
                 $car->save();
                 $car_id = $car->id;
                  $order = new Order();
