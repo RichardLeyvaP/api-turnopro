cabac215207500d84a3bd5c4b0f53c04929321d9
jimmbo89 <65252506+jimmbo89@users.noreply.github.com>
2024-01-05 12:39:06 -0500

    Update WorkplaceController.php

--- app/Http/Controllers/WorkplaceController.php
+++ app/Http/Controllers/WorkplaceController.php
@@ -15,7 +15,7 @@ class WorkplaceController extends Controller
     {
         try {
             Log::info("mostrar locales");
-            return response()->json(['workplaces' => Workplace::with(['professional','branch'])->get()], 200);
+            return response()->json(['workplaces' => Workplace::with(['branch'])->get()], 200);
         } catch (\Throwable $th) {
             return response()->json(['msg' => "Error al mostrar los Locales de Trabajo"], 500);
         }
@@ -29,7 +29,7 @@ class WorkplaceController extends Controller
             $workplace_data = $request->validate([
                 'id' => 'required|numeric'
             ]);
-            return response()->json(['workplaces' => Workplace::with(['professional','branch'])->find($workplace_data['id'])], 200);
+            return response()->json(['workplaces' => Workplace::with(['branch'])->find($workplace_data['id'])], 200);
         } catch (\Throwable $th) {
             return response()->json(['msg' => "Error al mostrar el Local de Trabajo"], 500);
         }
@@ -44,14 +44,12 @@ class WorkplaceController extends Controller
             $workplace_data = $request->validate([
                 'name' => 'required|max:100|unique:workplaces',
                 'busy' => 'required',
-                'professional_id' => 'required|numeric',
                 'branche_id' => 'required|numeric',
             ]);
 
             $workplace = new Workplace();
             $workplace->name = $workplace_data['name'];
             $workplace->busy = $workplace_data['busy'];
-            $workplace->professional_id = $workplace_data['professional_id'];
             $workplace->branche_id = $workplace_data['branche_id'];
             $workplace->save();
 
@@ -72,18 +70,16 @@ class WorkplaceController extends Controller
                 'id' => 'required|numeric',
                 'name' => 'required|max:100',
                 'busy' => 'required',
-                'professional_id' => 'required|numeric',
                 'branche_id' => 'required|numeric',
             ]);
 
             $workplace = Workplace::find($workplace_data['id']);
             $workplace->name = $workplace_data['name'];
             $workplace->busy = $workplace_data['busy'];
-            $workplace->professional_id = $workplace_data['professional_id'];
             $workplace->branche_id = $workplace_data['branche_id'];
             $workplace->save();
 
-            return response()->json(['msg' => 'Local de Trabajo actualizad0 correctamente'], 200);
+            return response()->json(['msg' => 'Local de Trabajo actualizado correctamente'], 200);
         } catch (\Throwable $th) {
             Log::info($th);
             return response()->json(['msg' => 'Error al actualizar el Local de Trabajo'], 500);
