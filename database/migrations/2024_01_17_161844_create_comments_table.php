8b3473b64f0f049c1b8446963c08cb6f6d73d127
jimmbo89 <65252506+jimmbo89@users.noreply.github.com>
2024-01-17 11:26:45 -0500

    Create 2024_01_17_161844_create_comments_table.php

---
+++ database/migrations/2024_01_17_161844_create_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('client_professional_id');
+            $table->date('data');
+            $table->string('look');
+
+            $table->foreign('client_professional_id')->references('id')->on('client_professional')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
