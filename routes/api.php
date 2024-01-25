39fac670c6834aad3f7b864a80ab95691ce35980
RichardLeyvaP <richardleyvap1991@gmail.com>
2024-01-25 17:06:01 -0300

    agregada nombre de carpeta coordinator

--- routes/api.php
+++ routes/api.php
@@ -284,7 +284,7 @@ Route::get('/images/{foldername}/{filename}', function ($foldername, $filename) 
     $response->header("Content-Type", $type);
 
     return $response;
-})->where(['folder' => 'professionals|clients|comments|products|services', 'filename' => '.*']);
+})->where(['folder' => 'professionals|clients|comments|products|services|coordinator', 'filename' => '.*']);
 
 
 
