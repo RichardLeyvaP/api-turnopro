517783dc3fb397b3b92f8c0b7c1ef8063c07be7f
jimmbo89 <65252506+jimmbo89@users.noreply.github.com>
2024-02-21 22:37:30 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -299,7 +299,7 @@ Route::get('/images/{foldername}/{filename}', function ($foldername, $filename) 
     $response->header("Content-Type", $type);
 
     return $response;
-})->where(['folder' => 'professionals|clients|comments|products|services|branches|images', 'filename' => '.*']);
+})->where(['folder' => 'professionals|clients|comments|products|services|branches|image', 'filename' => '.*']);
 
 
 
