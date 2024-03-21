bf9186bb2b06ee1149b207ec67e4fe0a1917e4e2
jimmbo89 <65252506+jimmbo89@users.noreply.github.com>
2024-03-21 18:57:36 -0400

    Update ProfessionalService.php

--- app/Services/ProfessionalService.php
+++ app/Services/ProfessionalService.php
@@ -73,10 +73,6 @@ class ProfessionalService
                 $type = 0;
                 $name = '';
                 $professional_id = 0;
-            }else{
-                $type = 3;
-                $name = $professionals->name.' '.$professionals->surname.' '.$professionals->second_surname;
-                $professional_id = $professionals->id;
             }
             return [
                 'name' => $name,
