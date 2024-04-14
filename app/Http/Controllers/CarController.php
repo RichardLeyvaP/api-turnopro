df754d342a0d969af961c15981f4626de28a4080
jimmbo89 <65252506+jimmbo89@users.noreply.github.com>
2024-04-14 10:41:00 -0400

    Update CarController.php

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -232,9 +232,9 @@ class CarController extends Controller
             $branch = Branch::where('id',$data['branch_id'])->first();
             Log::info( "Esta es la sucursal");
             Log::info( $branch);
-            $cars = $branch->cars()->with(['clientProfessional.client', 'clientProfessional.professional', 'payment'])->whereHas('reservation', function ($query) use ($data){
+            $cars = Car::whereHas('reservation', function ($query) use ($data){
                 $query->where('branch_id', $data['branch_id'])->whereDate('data', Carbon::now());
-            })->get()->map(function ($car) use ($data){
+            })->with(['clientProfessional.client', 'clientProfessional.professional', 'payment'])->get()->map(function ($car) use ($data){
                 $client = $car->clientProfessional->client;
                 $professional = $car->clientProfessional->professional;
                 $products = $car->orders->where('is_product', 1)->sum('price');
