5ffea86ed4fd0904c0602dd1f9e71dff9baeb669
jimmbo89 <65252506+jimmbo89@users.noreply.github.com>
2024-04-15 04:47:44 -0400

    Update ReservationService.php

--- app/Services/ReservationService.php
+++ app/Services/ReservationService.php
@@ -240,51 +240,6 @@ class ReservationService
                 'cantMaxService' => $services->max('orders_count')
             ];
         } else {
-            // Manejar la situación cuando el objeto es nulo
-            /*$clientProfessional = ClientProfessional::where('client_id', $data['client_id'])->first();
-
-            if ($clientProfessional) {
-                $professional = $clientProfessional->professional;   
-            }
-            $result = [
-                'clientName' => $client->name." ".$client->surname, 
-               'professionalName' => 'No ha sido atendido',
-                'image_url' => 'professionals/default_profile.jpg',
-                'imageLook' => 'comments/default_profile.jpg',             
-                'cantVisit' => $reservations->count(),
-                'endLook' => $comment ? $comment->look : null,
-                'frecuencia' => $fiel ? $fiel : $frecuencia,
-                'services' => $services->map(function ($service) use ($cantMaxService){
-                    return [
-                        'id' => $service->id,
-                        'name' => $service->name,
-                        'simultaneou' => $service->simultaneou,
-                        'price_service' => $service->price_service,
-                        'type_service' => $service->type_service,
-                        'profit_percentaje' => $service->profit_percentaje,
-                        'duration_service' => $service->duration_service,
-                        'image_service' => $service->image_service,
-                        'service_comment' => $service->service_comment,
-                        'cant' => $service->orders_count
-                    ];
-                }),
-                'products' => $products->map(function ($product){
-                    return [
-                        'id' => $product->id,
-                        'name' => $product->name,
-                        'code' => $product->code,
-                        'description' => $product->description,
-                        'product_exit' => 0,//solo para utilizar el modelo en apk bien,
-                        'status_product' => $product->status_product,
-                        'purchase_price' => $product->purchase_price,
-                        'sale_price' => $product->sale_price,
-                        'created_at' => $product->created_at,
-                        'updated_at' => $product->updated_at,
-                        'cant' => $product->orders_count
-                    ];
-                }),
-                'cantMaxService' => $services->max('orders_count')
-              ];*/
             return  $result = [
                 'clientName' => '',
                 'professionalName' => '',
