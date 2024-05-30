0b6845d9093ad5cf9ecba16bd0abb57b88e8d41d
RichardLeyvaP <richardleyvap1991@gmail.com>
2024-05-30 00:35:32 -0300

    configurando las tareas programadas

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,8 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Illuminate\Support\Facades\Log;
+use GuzzleHttp\Client;
+use Exception;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,19 +15,100 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // /home/simplif1/public_html/api2.simplifies.cl
-        // * * * * * cd /home/simplif1/public_html/api2.simplifies.cl && /opt/alt/php81/usr/bin/php artisan schedule:run >> /dev/null 2>&1
+        // Definir la primera tarea-Limpia la cola del dia anterior
+        $schedule->call(function () {
+            Log::info('Iniciando la primera tarea programada.Limpia la cola del dia anterior');
 
-        // /opt/alt/php81/usr/bin/php /home/simplif1/public_html/api2.simplifies.cl/artisan app:load_tail
+            // Crear un cliente HTTP
+            $client = new Client();
+
+            try {
+                // Hacer una solicitud GET a la primera ruta completa de la API
+                $response = $client->get('https://api2.simplifies.cl/api/cola_truncate');
+
+                // Verificar la respuesta
+                if ($response->getStatusCode() == 200) {
+                    Log::info('La solicitud a /cola_truncate se ejecutó correctamente.');
+                } else {
+                    Log::error('Error al ejecutar la solicitud a /cola_truncate: ' . $response->getStatusCode());
+                }
+            } catch (Exception $e) {
+                Log::error('Excepción al hacer la solicitud a /cola_truncate: ' . $e->getMessage());
+            }
+        })->dailyAt('01:00'); // Primera tarea a las 1:00 AM
+
+        // Definir la segunda tarea, Limpiar los puestos de trabajo
+        $schedule->call(function () {
+            Log::info('Iniciando la segunda tarea programada.Limpiar los puestos de trabajo');
+
+            // Crear un cliente HTTP
+            $client = new Client();
+
+            try {
+                // Hacer una solicitud GET a la segunda ruta completa de la API
+                $response = $client->get('https://api2.simplifies.cl/api/workplace-reset');
+
+                // Verificar la respuesta
+                if ($response->getStatusCode() == 200) {
+                    Log::info('La solicitud a /workplace-reset se ejecutó correctamente.');
+                } else {
+                    Log::error('Error al ejecutar la solicitud a /workplace-reset: ' . $response->getStatusCode());
+                }
+            } catch (Exception $e) {
+                Log::error('Excepción al hacer la solicitud a /workplace-reset: ' . $e->getMessage());
+            }
+        })->dailyAt('01:30'); // Segunda tarea a las 1:30 AM
+
+        
+        // Definir la tercera tarea-Envio de email de confirmación de reserva
+        $schedule->call(function () {
+            Log::info('Iniciando la tercera tarea programada.Envio de email de confirmación de reserva');
+
+            // Crear un cliente HTTP
+            $client = new Client();
+
+            try {
+                // Hacer una solicitud GET a la tercera ruta completa de la API
+                $response = $client->get('https://api2.simplifies.cl/api/reservation-send-mail');
+
+                // Verificar la respuesta
+                if ($response->getStatusCode() == 200) {
+                    Log::info('La solicitud a /reservation-send-mail se ejecutó correctamente.');
+                } else {
+                    Log::error('Error al ejecutar la solicitud a /reservation-send-mail: ' . $response->getStatusCode());
+                }
+            } catch (Exception $e) {
+                Log::error('Excepción al hacer la solicitud a /reservation-send-mail: ' . $e->getMessage());
+            }
+        })->dailyAt('06:00'); // Tercera tarea a las 6:00 AM
+
+        // Definir la cuarta tarea-Actualizar la cola del dia
+       $schedule->call(function () {
+            Log::info('Iniciando la tercera tarea programada.Actualizar la cola del dia');
+        // Crear un cliente HTTP
+         $client = new Client();
+         try {
+          // Hacer una solicitud GET a la tercera ruta completa de la API
+          $response = $client->get('https://api2.simplifies.cl/api/reservation_tail');
+
+        // Verificar la respuesta
+        if ($response->getStatusCode() == 200) {
+            Log::info('La solicitud a /reservation_tail se ejecutó correctamente.');
+        } else {
+            Log::error('Error al ejecutar la solicitud a /reservation_tail: ' . $response->getStatusCode());
+        }
+     } catch (Exception $e) {
+        Log::error('Excepción al hacer la solicitud a /reservation_tail: ' . $e->getMessage());
+     }
+})->dailyAt('07:00'); // Cuarta tarea a las 7:00 AM
 
 
-        $schedule->command('app:load_tail')->everyTenSeconds();
-        // $schedule->command('organize_queue')->everyTenSeconds();
-        // $schedule->call(function () {
-        //     Log::info("Funcion ejecutandose cada 10");
-        // })->everyTenSeconds();
-       // $schedule->command('task:execute 12:00')->daily(); // Ejemplo: Ejecutar todos los días a las 12:00
+        //
+        //
+        //
+        //
     }
+
 
     /**
      * Register the commands for the application.
@@ -33,7 +116,6 @@ class Kernel extends ConsoleKernel
     protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
-
         require base_path('routes/console.php');
     }
 }
